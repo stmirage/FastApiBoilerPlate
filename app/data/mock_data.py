6d96476934f76c893222955029cdee333cb5100b
stmirage <nik.luzgarev@yandex.ru>
2022-06-18 22:11:55 +0300

    added search route

---
+++ app/data/mock_data.py
@@ -0,0 +1,22 @@
+""" Тестовые данные для текущего шаблона приложения """
+
+TEST_DATA = [
+    {
+        "id": 1,
+        "label": "label_1",
+        "source": "source 1",
+        "url": "https://ya.ru/1"
+    },
+    {
+        "id": 2,
+        "label": "label_2",
+        "source": "source 2",
+        "url": "https://ya.ru/2"
+    },
+    {
+        "id": 3,
+        "label": "label_3",
+        "source": "source 1",
+        "url": "https://ya.ru/3"
+    }
+]
--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI, APIRouter
+from fastapi import FastAPI, APIRouter, HTTPException
+from .data.mock_data import TEST_DATA
+from typing import Optional
 
 app = FastAPI(title="BoilerPlate API", openapi_url="/openapi.json")
 
@@ -10,6 +12,33 @@ def root() -> dict:
     """ Root path """
 
     return {"msg": "Hello World"}
+
+
+@api_router.get("/record/{record_id}", status_code=200)
+def get_record(*, record_id: int) -> dict:
+    """ get single record """
+    
+    result = [record for record in TEST_DATA if record["id"] == record_id]
+    if result:
+        return result[0]
+    else:
+        raise HTTPException(status_code=404, detail="Record not found")
+
+
+@api_router.get("/search/", status_code=200)
+def search_records(
+    keyword: Optional[str] = None, max_results: Optional[int] = 10
+) -> dict:
+    """ default searching records method"""
+
+    if not keyword:
+        return {"results": TEST_DATA[:max_results]}
+    
+    results = list(filter(lambda record: keyword.lower() in record["label"].lower(), TEST_DATA))
+    
+    return results[:max_results]
+
+
 
 app.include_router(api_router)
 
